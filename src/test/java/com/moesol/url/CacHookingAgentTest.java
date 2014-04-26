dbaf5a8f9dda367375cf5d58a4f9e50d116e6455
Robert Hastings <rhastings@moesol.com>
2014-04-26 02:14:17 -0700

    Added unit test for windowsTrust.

--- src/test/java/com/moesol/url/CacHookingAgentTest.java
+++ src/test/java/com/moesol/url/CacHookingAgentTest.java
@@ -8,13 +8,18 @@ public class CacHookingAgentTest {
 
 	@Test
 	public void test() {
-		CacHookingAgent.processArgs("");
+		Config config = new Config();
+		CacHookingAgent.processArgs("", config);
 		assertEquals(false, CacHookingAgent.DEBUG);
 		assertEquals("TLS", CacHookingAgent.CONTEXT);
 		
-		CacHookingAgent.processArgs("load,debug,context=SSLv3");
+		CacHookingAgent.processArgs("load,debug,context=SSLv3", config);
 		assertEquals(true, CacHookingAgent.DEBUG);
 		assertEquals("SSLv3", CacHookingAgent.CONTEXT);
+		
+		config.setUseWindowsTrust(false);
+		CacHookingAgent.processArgs("load,debug,windowsTrust,context=SSLv3", config);
+		assertTrue(config.isUseWindowsTrust());
 	}
 
 }
