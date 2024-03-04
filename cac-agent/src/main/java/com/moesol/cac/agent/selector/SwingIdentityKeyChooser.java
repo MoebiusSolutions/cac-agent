7c14044831285632bed81262d6ecdbcedde7f9da
Robert Hastings (CTR) <rhastings@moesol.com>
2024-03-03 20:58:03 -0800

    Show in task list when dialogs open. Also, show icon in system tray. When system tray icon clicked bring dialog prompting to front.
    Under some circumstances the dialog
    could be covered by other windows
    and hard to find, or rarely, the dialog
    would be completely hidden.
    
    Tested under Windows 10 and Ubuntu 20.04.
    Windows supports both the task list and
    system tray features.
    Ubuntu no longer has a system tray,
    so that feature does not work on Ubuntu.
    Also, the title on Ubuntu shows up as
    "java-lang-Thread". However, it is still
    useful in helping to find the dialog
    if it is hidden.

--- cac-agent/src/main/java/com/moesol/cac/agent/selector/SwingIdentityKeyChooser.java
+++ cac-agent/src/main/java/com/moesol/cac/agent/selector/SwingIdentityKeyChooser.java
@@ -1,29 +1,38 @@
 package com.moesol.cac.agent.selector;
 
+import java.awt.AWTException;
 import java.awt.AWTKeyStroke;
 import java.awt.Component;
 import java.awt.Image;
 import java.awt.KeyboardFocusManager;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 import javax.swing.AbstractAction;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -34,17 +43,22 @@ import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
 public class SwingIdentityKeyChooser implements IdentityKeyChooser {
+	private static final String TITLE = "CAC Agent";
 	private final IdentityKeyListProvider provider;
 	private String applicationName;
 	private Component parentComponent;
+	private JFrame defaultParent;
+	private Timer maybeHideDefault = new Timer(1000, e -> hideDefault());
 	private IdentityKeyCertFormatter formatter;
 	private String choosenAlias;
 	private Timer maybeShowBusy = new Timer(1000, e -> showBusyNow());
 	private JDialog busy;
+	private boolean reportedSystemTrayUnsupported = false;
 	
 	public SwingIdentityKeyChooser(IdentityKeyListProvider provider) {
 		this.provider = provider;
 		this.formatter = DefaultCertFormatter.INSTANCE;
+		maybeHideDefault.start();
 	}
 	
 	public void setApplicationName(String applicationName) {
@@ -60,14 +74,12 @@ public class SwingIdentityKeyChooser implements IdentityKeyChooser {
 	}
 
 	public void showNoIdentitiesFound(final String remoteHost) {
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				String title = makeTitle("No Identities Found");
-				String message = "No certificates were found to authenticate to "
-					+ (remoteHost == null ? "the server." : remoteHost);
-				JOptionPane.showMessageDialog(getParent(), message, title, JOptionPane.WARNING_MESSAGE);
-			}
+		laterWithJFrame(frame -> {
+			String title = makeTitle("No Identities Found");
+			String message = "No certificates were found to authenticate to "
+				+ (remoteHost == null ? "the server." : remoteHost);
+			JOptionPane.showMessageDialog(frame, message, title, JOptionPane.WARNING_MESSAGE);
+			addTrayNotification(message, TrayIcon.MessageType.WARNING);
 		});
 	}
 
@@ -82,25 +94,20 @@ public class SwingIdentityKeyChooser implements IdentityKeyChooser {
 	}
 	
 	public void reportException(final Exception e) {
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				JOptionPane.showMessageDialog(getParent(), e.getLocalizedMessage(), makeTitle("Failed"), JOptionPane.ERROR_MESSAGE);
-			}
+		laterWithJFrame(frame -> {
+			String msg = e.getLocalizedMessage();
+			JOptionPane.showMessageDialog(frame, msg, makeTitle("Failed"), JOptionPane.ERROR_MESSAGE);
+			addTrayNotification(msg, TrayIcon.MessageType.ERROR);
 		});
 	}
 	
 	public void showBusy(final String message) {
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				JOptionPane pane = new JOptionPane(message, JOptionPane.INFORMATION_MESSAGE);
-				busy = pane.createDialog(parentComponent, makeTitle("CAC"));
-				setWindowIcon(busy);
-				busy.setModal(false);
-			}
-		});
-		
+		JOptionPane pane = new JOptionPane(message, JOptionPane.INFORMATION_MESSAGE);
+		busy = pane.createDialog(getParent(), makeTitle(TITLE));
+		setWindowIcon(busy);
+		addTrayNotification(message, TrayIcon.MessageType.INFO);
+		busy.setModal(false);
+
 		maybeShowBusy.setInitialDelay(3000);
 		maybeShowBusy.setRepeats(false);
 		maybeShowBusy.start();
@@ -134,8 +141,10 @@ public class SwingIdentityKeyChooser implements IdentityKeyChooser {
 		pane.setMessage(panel);
 		pane.setMessageType(JOptionPane.QUESTION_MESSAGE);
 		pane.setOptionType(JOptionPane.OK_CANCEL_OPTION);
-		JDialog dialog = pane.createDialog(busy, title);
+		JDialog dialog = pane.createDialog(getParent(), title);
 		setWindowIcon(dialog);
+		addTrayNotification("PIN Required", TrayIcon.MessageType.INFO);
+
 		dialog.addWindowFocusListener(new WindowAdapter() {
 		    public void windowGainedFocus(WindowEvent e) {
 		    	pass.requestFocusInWindow();
@@ -151,7 +160,7 @@ public class SwingIdentityKeyChooser implements IdentityKeyChooser {
 		Object result = pane.getValue();
 		dialog.dispose();
 		
-		if (new Integer(0).equals(result)) {
+		if (Integer.valueOf(0).equals(result)) {
 			return pass.getPassword();
 		}
 		return null;
@@ -199,12 +208,12 @@ public class SwingIdentityKeyChooser implements IdentityKeyChooser {
 
 		final JDialog dialog = pane.createDialog(getParent(), makeTitle("Select Identity"));
 		setWindowIcon(dialog);
+		addTrayNotification("Select Identify", TrayIcon.MessageType.INFO);
 		bindArrowKeys(dialog);
 		panel.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(KeyStroke.getKeyStroke("ENTER"), "selectKey");
 		panel.getActionMap().put("selectKey", new AbstractAction() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("selectKey");
 				if (dialog.getFocusOwner() instanceof JButton) {
 					JButton jb = (JButton) dialog.getFocusOwner();
 					jb.doClick();
@@ -251,13 +260,13 @@ public class SwingIdentityKeyChooser implements IdentityKeyChooser {
 
 	@Override
 	public void promptForCardInsertion(String title, String error) {
-		String msg = error + "\n\nInsert Smartcard";
-//		JOptionPane.showMessageDialog(null, msg);
+		String msg = error + "\n\nInsert Smart Card";
 		
 	    Object stringArray[] = { "OK", "Exit" };
-	    int r = JOptionPane.showOptionDialog(busy, msg, title, 
+	    int r = JOptionPane.showOptionDialog(getParent(), msg, title, 
 	    		JOptionPane.YES_NO_OPTION, 
 	    		JOptionPane.QUESTION_MESSAGE, null, stringArray, stringArray[0]);
+		addTrayNotification(msg, TrayIcon.MessageType.INFO);
 	    switch (r) {
 	    case JOptionPane.OK_OPTION:
 	    	break;
@@ -270,10 +279,45 @@ public class SwingIdentityKeyChooser implements IdentityKeyChooser {
 	}
 
 	protected Component getParent() {
-		if (busy != null && busy.isShowing()) {
-			return busy;
-		} else {
+		if (parentComponent != null) {
 			return parentComponent;
+		}
+		if (defaultParent != null) {
+			defaultParent.setVisible(true);
+			return defaultParent;
+		}
+
+		JFrame frame = new JFrame(TITLE);
+		frame.setUndecorated(true);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+
+		defaultParent = frame;
+		return frame;
+	}
+	/**
+	 * Maybe hide and dispose of defaultParent.
+	 * Skip if defaultParent is not in use.
+	 * Otherwise, when we detect all of our dialogs are closed,
+	 * we can hide and dispose of the defaultParent.
+	 */
+	private void hideDefault() {
+		if (defaultParent == null) {
+			return;
+		}
+		int visible = 0;
+		for (Window w : defaultParent.getOwnedWindows()) {
+			if (w == defaultParent) {
+				continue;
+			}
+			if (w.isShowing()) {
+				visible++;
+			}
+		}
+		if (visible == 0) {
+			defaultParent.setVisible(false);
+			defaultParent.dispose();
+			defaultParent = null;
 		}
 	}
 
@@ -300,5 +344,64 @@ public class SwingIdentityKeyChooser implements IdentityKeyChooser {
 			}
 		}
 	}
+
+	private void laterWithJFrame(Consumer<Component> consumer) {
+		SwingUtilities.invokeLater(() -> {
+			consumer.accept(getParent());
+		});
+	}
+
+	private void addTrayNotification(String msg, TrayIcon.MessageType type) {
+		if (!SystemTray.isSupported()) {
+			if (reportedSystemTrayUnsupported) {
+				return;
+			}
+			System.out.println("SystemTray is not supported");
+			reportedSystemTrayUnsupported = true;
+			return;
+		}
+		Image image = createImage("/smart-card-icon.png", TITLE);
+		if (image == null) {
+			System.err.println("createImage failed");
+			return;
+		}
+		SystemTray tray = SystemTray.getSystemTray();
+		TrayIcon[] icons = tray.getTrayIcons();
+		TrayIcon trayIcon = null;
+		if (icons.length == 0) {
+			try {
+				trayIcon = new TrayIcon(image, applicationName);
+				trayIcon.setImageAutoSize(true);
+				trayIcon.setToolTip(TITLE);
+				trayIcon.addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent e) {
+						if (defaultParent == null) {
+							return;
+						}
+						defaultParent.toFront();
+						defaultParent.requestFocus();
+					}
+				});
+				tray.add(trayIcon);
+			} catch (AWTException e) {
+				e.printStackTrace(System.err);
+			}
+		} else {
+			trayIcon = icons[0];
+		}
+		if (trayIcon != null) {
+			trayIcon.displayMessage(TITLE, msg, type);
+		}
+	}
+	protected static Image createImage(String path, String description) {
+        URL imageURL = SwingIdentityKeyChooser.class.getResource(path);
+         
+        if (imageURL == null) {
+            System.err.println("Resource not found: " + path);
+            return null;
+        } else {
+            return (new ImageIcon(imageURL, description)).getImage();
+        }
+    }
 
 }
